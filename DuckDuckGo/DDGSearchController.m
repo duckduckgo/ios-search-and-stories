dc4c6d1fab4c852f57701c68a65e059cc1e0af61
Ishaan Gulrajani <igul222@gmail.com>
2012-05-13 15:29:42 -0400

    Autocomplete: tweak history cell style

--- DuckDuckGo/DDGSearchController.m
+++ DuckDuckGo/DDGSearchController.m
@@ -364,53 +364,69 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tv cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *suggestionCellID = @"SCell";
+    static NSString *historyCellID = @"HCell";
     
-    UITableViewCell *cell = [tv dequeueReusableCellWithIdentifier:CellIdentifier];
-	UIImageView *iv;
-    if (cell == nil)
-	{
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
-		cell.textLabel.font = [UIFont boldSystemFontOfSize:15.0];
-		cell.textLabel.textColor = [UIColor darkGrayColor];
-		cell.selectionStyle = UITableViewCellSelectionStyleGray;
-		cell.imageView.image = [UIImage imageNamed:@"spacer44x44.png"];
-		cell.backgroundView = [[UIView alloc] init];
-        [cell.backgroundView setBackgroundColor:[UIColor whiteColor]];
-        
-		iv = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 44.0, 44.0)];
-		iv.tag = 100;
-		iv.contentMode = UIViewContentModeScaleAspectFill;
-		iv.clipsToBounds = YES;
-        iv.backgroundColor = [UIColor whiteColor];
-		[cell.contentView addSubview:iv];
-    }
-
     NSArray *history = [historyProvider pastHistoryItemsForPrefix:searchField.text];
     NSArray *suggestions = [suggestionsProvider suggestionsForSearchText:searchField.text];
     if((suggestions.count + history.count) <= indexPath.row)
-        return cell; // this entry no longer exists; return empty cell. the tableview will be reloading very soon anyway.
-
-    iv = (UIImageView *)[cell.contentView viewWithTag:100];
-    iv.backgroundColor = [UIColor whiteColor];
-
+        return [tv dequeueReusableCellWithIdentifier:suggestionCellID]; // this entry no longer exists; return empty cell. the tableview will be reloading very soon anyway.
+    
+    UITableViewCell *cell;
     if(indexPath.row < history.count) {
+        // dequeue or initialize a history cell
+        cell = [tv dequeueReusableCellWithIdentifier:historyCellID];
+        UIImageView *iv;
+        if(cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:historyCellID];
+            cell.textLabel.font = [UIFont boldSystemFontOfSize:15.0];
+            cell.textLabel.textColor = [UIColor darkGrayColor];
+            cell.selectionStyle = UITableViewCellSelectionStyleGray;
+            cell.backgroundView = [[UIView alloc] init];
+            [cell.backgroundView setBackgroundColor:[UIColor whiteColor]];
+        }
+        
+        // fill the appropriate data into the history cell
         NSDictionary *historyItem = [history objectAtIndex:indexPath.row];
         cell.textLabel.text = [historyItem objectForKey:@"text"];
-        cell.detailTextLabel.text = @"History item";
+        //cell.detailTextLabel.text = @"History item";
         [iv setImage:nil];
+        
     } else {
-     	NSDictionary *item = [suggestions objectAtIndex:indexPath.row - history.count];
+        // dequeue or initialize a suggestion cell
+        cell = [tv dequeueReusableCellWithIdentifier:suggestionCellID];
+        UIImageView *iv;
+        if(cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:historyCellID];
+            cell.textLabel.font = [UIFont boldSystemFontOfSize:15.0];
+            cell.textLabel.textColor = [UIColor darkGrayColor];
+            cell.selectionStyle = UITableViewCellSelectionStyleGray;
+            cell.imageView.image = [UIImage imageNamed:@"spacer44x44.png"];
+            cell.backgroundView = [[UIView alloc] init];
+            [cell.backgroundView setBackgroundColor:[UIColor whiteColor]];
+            
+            iv = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 44.0, 44.0)];
+            iv.tag = 100;
+            iv.contentMode = UIViewContentModeScaleAspectFill;
+            iv.clipsToBounds = YES;
+            iv.backgroundColor = [UIColor whiteColor];
+            [cell.contentView addSubview:iv];
+        } else {
+            iv = (UIImageView *)[cell.contentView viewWithTag:100];
+        }
         
-        cell.textLabel.text = [item objectForKey:ksDDGSearchControllerServerKeyPhrase];
-        cell.detailTextLabel.text = [item objectForKey:ksDDGSearchControllerServerKeySnippet];
+     	NSDictionary *suggestionItem = [suggestions objectAtIndex:indexPath.row - history.count];
         
-        if([item objectForKey:ksDDGSearchControllerServerKeyImage])
-            [iv setImageWithURL:[NSURL URLWithString:[item objectForKey:ksDDGSearchControllerServerKeyImage]]];
+        cell.textLabel.text = [suggestionItem objectForKey:ksDDGSearchControllerServerKeyPhrase];
+        cell.detailTextLabel.text = [suggestionItem objectForKey:ksDDGSearchControllerServerKeySnippet];
+        
+        if([suggestionItem objectForKey:ksDDGSearchControllerServerKeyImage])
+            [iv setImageWithURL:[NSURL URLWithString:[suggestionItem objectForKey:ksDDGSearchControllerServerKeyImage]]];
         else
             [iv setImage:nil]; // wipe out any image that used to be there
    
-    }    
+    }
+    
     return cell;
 }
 
