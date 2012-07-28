587ad18be228c78e2dd4cb3436cea8c03db30adc
Ishaan Gulrajani <igul222@gmail.com>
2012-07-28 17:57:19 -0400

    Show popover background and arrow

--- DuckDuckGo/DDGSearchController.m
+++ DuckDuckGo/DDGSearchController.m
@@ -119,23 +119,63 @@
     }
 }
 
-#pragma  mark - Interactions with search handler
-- (IBAction)leftButtonPressed:(UIButton*)sender {
-	[_searchField resignFirstResponder];
-    
-    // if it's showing, hide it.
-    [self revealAutocomplete:NO];
-    
-	[_searchHandler searchControllerLeftButtonPressed];
+#pragma mark - Popover
+
+- (WEPopoverContainerViewProperties *)improvedContainerViewProperties {
+	WEPopoverContainerViewProperties *props = [[WEPopoverContainerViewProperties alloc] init];
+	NSString *bgImageName = nil;
+	CGFloat bgMargin = 0.0;
+	CGFloat bgCapSize = 0.0;
+	CGFloat contentMargin = 4.0;
+	
+	bgImageName = @"popoverBg.png";
+	
+	// These constants are determined by the popoverBg.png image file and are image dependent
+	bgMargin = 13; // margin width of 13 pixels on all sides popoverBg.png (62 pixels wide - 36 pixel background) / 2 == 26 / 2 == 13
+	bgCapSize = 31; // ImageSize/2  == 62 / 2 == 31 pixels
+	
+	props.leftBgMargin = bgMargin;
+	props.rightBgMargin = bgMargin;
+	props.topBgMargin = bgMargin;
+	props.bottomBgMargin = bgMargin;
+	props.leftBgCapSize = bgCapSize;
+	props.topBgCapSize = bgCapSize;
+	props.bgImageName = bgImageName;
+	props.leftContentMargin = contentMargin;
+	props.rightContentMargin = contentMargin - 1; // Need to shift one pixel for border to look correct
+	props.topContentMargin = contentMargin;
+	props.bottomContentMargin = contentMargin;
+	
+	props.arrowMargin = 4.0;
+	
+	props.upArrowImageName = @"popoverArrowUp.png";
+	props.downArrowImageName = @"popoverArrowDown.png";
+	props.leftArrowImageName = @"popoverArrowLeft.png";
+	props.rightArrowImageName = @"popoverArrowRight.png";
+	return props;
 }
 
 - (IBAction)actionButtonPressed:(id)sender {
     DDGPopoverViewController *popoverViewController = [[DDGPopoverViewController alloc] initWithNibName:nil bundle:nil];
     self.wePopoverController = [[WEPopoverController alloc] initWithContentViewController:popoverViewController];
-    [_wePopoverController presentPopoverFromRect:[_parentView convertRect:_actionButton.frame fromView:self.view]
-                                       inView:_parentView
-                     permittedArrowDirections:UIPopoverArrowDirectionUp
-                                     animated:YES];
+    _wePopoverController.containerViewProperties = [self improvedContainerViewProperties];
+    
+    CGRect popoverRect = _actionButton.frame;
+    popoverRect.origin.x -= 35; // correction for what is apparently a bug in WEPopover
+    [_wePopoverController presentPopoverFromRect:popoverRect
+                                          inView:self.view
+                        permittedArrowDirections:UIPopoverArrowDirectionUp
+                                        animated:YES];
+}
+
+#pragma  mark - Interactions with search handler
+
+- (IBAction)leftButtonPressed:(UIButton*)sender {
+	[_searchField resignFirstResponder];
+    
+    // if it's showing, hide it.
+    [self revealAutocomplete:NO];
+	[_searchHandler searchControllerLeftButtonPressed];
 }
 
 -(void)setState:(DDGSearchControllerState)searchControllerState {
