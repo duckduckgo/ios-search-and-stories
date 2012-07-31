029009578d82554ac1f75c907946eb3059e81631
Ishaan Gulrajani <igul222@gmail.com>
2012-07-30 23:38:38 -0400

    Add device info to feedback message

--- DuckDuckGo/DDGSettingsViewController.m
+++ DuckDuckGo/DDGSettingsViewController.m
@@ -11,6 +11,7 @@
 #import "DDGNewsSourcesViewController.h"
 #import "SHK.h"
 #import "SVProgressHUD.h"
+#import <sys/utsname.h>
 
 @implementation DDGSettingsViewController
 
@@ -54,11 +55,12 @@
         [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=479988136&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software"]];
     }];
     __weak DDGSettingsViewController *weakSelf = self;
-    [self addButton:@"Send Feedback" action:^{
+    [self addButton:@"Send feedback" action:^{
         MFMailComposeViewController *mailVC = [[MFMailComposeViewController alloc] init];
         mailVC.mailComposeDelegate = self;
-        [mailVC setToRecipients:@[@"ishaan@duckduckgo.com"]];
+        [mailVC setToRecipients:@[@"help@duckduckgo.com"]];
         [mailVC setSubject:@"DuckDuckGo app feedback"];
+        [mailVC setMessageBody:[NSString stringWithFormat:@"I'm running %@. Here's my feedback:",[weakSelf deviceInfo]] isHTML:NO];
         [weakSelf presentModalViewController:mailVC animated:YES];
     }];
 }
@@ -86,4 +88,32 @@
     [self dismissModalViewControllerAnimated:YES];
 }
 
+-(NSString *)deviceInfo {
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    NSString *device = [NSString stringWithCString:systemInfo.machine
+                                          encoding:NSUTF8StringEncoding];
+    NSDictionary *deviceNames = @{
+        @"x86_64"    : @"iOS simulator",
+        @"i386"      : @"iOS simulator",
+        @"iPod1,1"   : @"iPod touch 1G",
+        @"iPod2,1"   : @"iPod touch 2G",
+        @"iPod3,1"   : @"iPod touch 3G",
+        @"iPod4,1"   : @"iPod touch 4G",
+        @"iPhone1,1" : @"iPhone",
+        @"iPhone1,2" : @"iPhone 3G",
+        @"iPhone2,1" : @"iPhone 3GS",
+        @"iPad1,1"   : @"iPad",
+        @"iPad2,1"   : @"iPad 2",
+        @"iPhone3,1" : @"iPhone 4",
+        @"iPhone4,1" : @"iPhone 4S"
+    };
+    if([deviceNames objectForKey:device])
+        device = [deviceNames objectForKey:device];
+    
+    NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+    NSString *osVersion = [[UIDevice currentDevice] systemVersion];
+    
+    return [NSString stringWithFormat:@"DuckDuckGo v%@ on an %@ (iOS %@)",appVersion,device,osVersion];
+}
 @end
