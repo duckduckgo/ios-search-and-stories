1b557909b17f2c7bb15090b71f5847b294d6c5fe
Ishaan Gulrajani <igul222@gmail.com>
2012-08-01 20:33:41 -0400

    Switch back to live URLs

--- DuckDuckGo/Constants.m
+++ DuckDuckGo/Constants.m
@@ -9,12 +9,12 @@
 #import "Constants.h"
 
 // local testing URLs
-NSString * const kDDGTypeInfoURLString = @"http://localhost:8000/type_info.js?";
-NSString * const kDDGStoriesURLString = @"http://localhost:8000/stories.js?";
-NSString * const kDDGCustomStoriesURLString = @"http://localhost:8000/custom.js?";
-NSString * const kDDGSuggestionsURLString = @"http://localhost:8000/suggestions.json?";
+//NSString * const kDDGTypeInfoURLString = @"http://localhost:8000/type_info.js?";
+//NSString * const kDDGStoriesURLString = @"http://localhost:8000/stories.js?";
+//NSString * const kDDGCustomStoriesURLString = @"http://localhost:8000/custom.js?";
+//NSString * const kDDGSuggestionsURLString = @"http://localhost:8000/suggestions.json?";
 
-//NSString * const kDDGTypeInfoURLString = @"http://caine.duckduckgo.com/watrcoolr.js?o=json&type_info=1";
-//NSString * const kDDGStoriesURLString = @"http://caine.duckduckgo.com/watrcoolr.js?o=json&s=";
-//NSString * const kDDGCustomStoriesURLString = @"http://caine.duckduckgo.com/news.js?o=json&t=m&q=";
-//NSString * const kDDGSuggestionsURLString = @"http://swass.duckduckgo.com:6767/face/suggest/?q=";
+NSString * const kDDGTypeInfoURLString = @"http://caine.duckduckgo.com/watrcoolr.js?o=json&type_info=1";
+NSString * const kDDGStoriesURLString = @"http://caine.duckduckgo.com/watrcoolr.js?o=json&s=";
+NSString * const kDDGCustomStoriesURLString = @"http://caine.duckduckgo.com/news.js?o=json&t=m&q=";
+NSString * const kDDGSuggestionsURLString = @"http://swass.duckduckgo.com:6767/face/suggest/?q=";
