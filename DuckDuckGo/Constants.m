6c77b22d87d3ca5a655265284c6e3d0be4dc34da
Caine Tighe <caine@tinytim.duckduckgo.com>
2012-11-16 19:13:20 -0500

    Constants.m: update watrcoolr location

--- DuckDuckGo/Constants.m
+++ DuckDuckGo/Constants.m
@@ -14,7 +14,7 @@
 //NSString * const kDDGCustomStoriesURLString = @"http://localhost:8000/custom.js?";
 //NSString * const kDDGSuggestionsURLString = @"http://localhost:8000/suggestions.json?";
 
-NSString * const kDDGTypeInfoURLString = @"http://caine.duckduckgo.com/watrcoolr.js?o=json&type_info=1";
-NSString * const kDDGStoriesURLString = @"http://caine.duckduckgo.com/watrcoolr.js?o=json&s=";
-NSString * const kDDGCustomStoriesURLString = @"http://caine.duckduckgo.com/news.js?o=json&t=m&q=";
+NSString * const kDDGTypeInfoURLString = @"http://watrcoolr.duckduckgo.com/watrcoolr.js?o=json&type_info=1";
+NSString * const kDDGStoriesURLString = @"http://watrcoolr.duckduckgo.com/watrcoolr.js?o=json&s=";
+NSString * const kDDGCustomStoriesURLString = @"http://duckduckgo.com/news.js?o=json&t=m&q=";
 NSString * const kDDGSuggestionsURLString = @"http://swass.duckduckgo.com:6767/face/suggest/?q=";
