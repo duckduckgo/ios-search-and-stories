f88d62aa4505efce4777760e0b13cbce1cc32313
Chris Heimark <cjheimark@gmail.com>
2012-11-30 16:31:41 -0500

    Added spreadsheet containing keys for ShareKit and access information for accounts setup. Not all keys are yet verified but they are at least created and there are accounts for those platforms.

--- DuckDuckGo/DDGSHKConfigurator.m
+++ DuckDuckGo/DDGSHKConfigurator.m
@@ -26,4 +26,72 @@
 	return @"208114382551369";
 }
 
+
+// Read It Later - http://readitlaterlist.com/api/signup/ NOW http://getpocket.com/
+- (NSString*)readItLaterKey
+{
+	return @"10907-2685d2ee57602fef03e3da7c";
+}
+
+// LinkedIn - https://www.linkedin.com/secure/developer
+- (NSString*)linkedInConsumerKey
+{
+	return @"4yjfvmpaypq4";
+}
+
+- (NSString*)linkedInSecret
+{
+	return @"OXXO1tfvcRg7UaT7";
+}
+
+- (NSString*)linkedInCallbackUrl
+{
+	return @"";
+}
+
+// Readability - http://www.readability.com/publishers/api/
+- (NSString*)readabilityConsumerKey
+{
+	return @"";
+}
+
+- (NSString*)readabilitySecret
+{
+	return @"";
+}
+
+// Evernote - http://www.evernote.com/about/developer/api/
+/*	You need to set to sandbox until you get approved by evernote. If you use sandbox, you can use it with special sandbox user account only. You can create it here: https://sandbox.evernote.com/Registration.action
+ If you already have a consumer-key and secret which have been created with the old username/password authentication system
+ (created before May 2012) you have to get a new consumer-key and secret, as the old one is not accepted by the new authentication
+ system.
+ // Sandbox
+ #define SHKEvernoteHost    @"sandbox.evernote.com"
+ 
+ // Or production
+ #define SHKEvernoteHost    @"www.evernote.com"
+ */
+
+- (NSString*)evernoteHost
+{
+	return @"https://sandbox.evernote.com/";
+}
+
+- (NSString*)evernoteConsumerKey
+{
+	return @"duckduckgo";
+}
+
+- (NSString*)evernoteSecret
+{
+	return @"2049b7f87a63686b";
+}
+
+// Diigo - http://www.diigo.com/api_keys/new/
+- (NSString*)diigoKey
+{
+	return @"1c521ddb317315b3";
+}
+
+
 @end
