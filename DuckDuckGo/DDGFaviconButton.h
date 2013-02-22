b1e7d8f26f127c82a8eb62db9b99d19cf5b88a8b
Johnnie Walker <mrwalker@randomsequence.com>
2013-02-22 13:07:27 +0000

    Fix blurry favicons

---
+++ DuckDuckGo/DDGFaviconButton.h
@@ -0,0 +1,13 @@
+//
+//  DDGFaviconButton.h
+//  DuckDuckGo
+//
+//  Created by Johnnie Walker on 22/02/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDGFaviconButton : UIButton
+
+@end
---
+++ DuckDuckGo/DDGFaviconButton.m
@@ -0,0 +1,31 @@
+//
+//  DDGFaviconButton.m
+//  DuckDuckGo
+//
+//  Created by Johnnie Walker on 22/02/2013.
+//
+//
+
+#import "DDGFaviconButton.h"
+
+@implementation DDGFaviconButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
