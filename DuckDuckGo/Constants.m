5f82e74ff183b2637d67c183f89bda46b2f722d6
Caine Tighe <caine@duckduckgo.com>
2013-05-08 21:02:31 -0400

    Constants.m: Need rundmc back for Mobile Summit demo.

--- DuckDuckGo/Constants.m
+++ DuckDuckGo/Constants.m
@@ -16,12 +16,12 @@
 
 NSString * const kDDGCustomStoriesURLString = @"https://duckduckgo.com/news.js?o=json&t=m&q=";
 
-//#if DEBUG == 1
+#if DEBUG == 1
 //NSString * const kDDGTypeInfoURLString = @"https://caine.duckduckgo.com/watrcoolr.js?o=json&type_info=1";
 //NSString * const kDDGStoriesURLString = @"https://caine.duckduckgo.com/watrcoolr.js?o=json&s=";
-//NSString * const kDDGSuggestionsURLString = @"http://rundmc.duckduckgo.com:6767/?q=";
-//#else
+NSString * const kDDGSuggestionsURLString = @"http://rundmc.duckduckgo.com:6767/?q=";
+#else
 NSString * const kDDGTypeInfoURLString = @"https://watrcoolr.duckduckgo.com/watrcoolr.js?o=json&type_info=1";
 NSString * const kDDGStoriesURLString = @"https://watrcoolr.duckduckgo.com/watrcoolr.js?o=json&s=";
 NSString * const kDDGSuggestionsURLString = @"https://duckduckgo.com/ac/?q=";
-//#endif
+#endif
