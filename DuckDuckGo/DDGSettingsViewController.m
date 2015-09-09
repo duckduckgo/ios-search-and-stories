3722dff83dc6276cf3136572b8b1409cd120a2b9
Sean Reilly <sreilly@infinitekind.com>
2015-09-09 13:39:39 +0100

    re-add setting for the default "home" view

--- DuckDuckGo/DDGSettingsViewController.m
+++ DuckDuckGo/DDGSettingsViewController.m
@@ -118,7 +118,15 @@ NSString * const DDGSettingHomeViewTypeDuck = @"Duck Mode";
     __weak DDGSettingsViewController *weakSelf = self;
     
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-
+    
+    [self addSectionWithTitle:@"Home" footer:nil];
+    
+    NSString *homeViewMode = [defaults objectForKey:DDGSettingHomeView];
+    [self addRadioOptionWithTitle:@"Stories" value:DDGSettingHomeViewTypeStories key:DDGSettingHomeView selected:[homeViewMode isEqual:DDGSettingHomeViewTypeStories]];
+    [self addRadioOptionWithTitle:@"Recent" value:DDGSettingHomeViewTypeRecents key:DDGSettingHomeView selected:[homeViewMode isEqual:DDGSettingHomeViewTypeRecents]];
+    [self addRadioOptionWithTitle:@"Favorites" value:DDGSettingHomeViewTypeSaved key:DDGSettingHomeView selected:[homeViewMode isEqual:DDGSettingHomeViewTypeSaved]];
+    [self addRadioOptionWithTitle:@"Search Only" value:DDGSettingHomeViewTypeDuck key:DDGSettingHomeView selected:[homeViewMode isEqual:DDGSettingHomeViewTypeDuck]];
+    
     [self addSectionWithTitle:@"Stories" footer:nil];
     [self addButton:@"Sources" forKey:@"sources" detailTitle:nil type:IGFormButtonTypeDisclosure action:^{
         DDGChooseSourcesViewController *sourcesVC = [[DDGChooseSourcesViewController alloc] initWithStyle:UITableViewStyleGrouped];
