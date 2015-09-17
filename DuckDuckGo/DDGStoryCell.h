30bab45bfba1ac43af63215253d2e336adaec9cb
Sean Reilly <sreilly@infinitekind.com>
2015-09-17 21:51:46 +0100

    replace DDGTitleBarHeight const with a #define

--- DuckDuckGo/DDGStoryCell.h
+++ DuckDuckGo/DDGStoryCell.h
@@ -9,10 +9,9 @@
 #import <UIKit/UIKit.h>
 #import "DDGStory.h"
 
-extern NSString *const DDGStoryCellIdentifier;
+#define DDGTitleBarHeight 57.0f
 
-CGFloat const DDGTitleBarHeight = 57.0f;
-CGFloat const DDGTitleBarHeightRatio = 240.0f/740.0f; // 240/740 == 0.324324324 == mosaicmode,    114/475 == 0.24 == normalmode   other: 114/360
+extern NSString *const DDGStoryCellIdentifier;
 
 @protocol DDGStoryCellDelegate <NSObject>
 
